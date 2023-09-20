61b00ab56efef06e9a04665ba1005330e7f08680
somata <somata@foxmail.com>
2023-09-20 13:16:11 +0800

    代码重构; 支持一条命令同时上传多个文件

--- uploader.py
+++ uploader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""思源助手主程序, 用于将本地Markdown上传至思源笔记本。
+"""思源上传助手主程序, 用于将本地Markdown上传至思源笔记本。
 
 file: /uploader.py
 author: somata
@@ -13,29 +13,19 @@ date: 2023-7-31
 import re
 from os.path import join as pathjoin
 from os.path import basename, normcase, dirname
-from sys import argv, stderr
+from sys import argv,stderr
 from sys import exit as broken
 from loguru import logger
 from api import Siyuan
 from config import URL, TOKEN, VERIFY
 
-logger.remove()
-logger.add(stderr, level="INFO")
+USAGE = """文件上传助手, 请在 config.py 配置基础信息
+usage: python3 ./uploader.py <FILE[ FILE...]>
+"""
 
 
-@logger.catch
-def main():
-    """主函数
-    """
-    if len(argv) != 2:
-        logger.error("Please input filename.")
-        broken(129)
-
-    makrdown_file = argv[1]
-
-    siyuan = Siyuan(URL, TOKEN, VERIFY)
-
-    # 选择笔记本
+def choose_notebook(siyuan: Siyuan) -> str:
+    """交互式选择一个笔记本"""
     notebooks = siyuan.ls_notebooks()
     i = 0
     for notebook in notebooks:
@@ -43,8 +33,15 @@ def main():
         i += 1
     target_notebook_id = notebooks[int(input("请选择笔记本:"))][1]
     logger.debug(f"target notebook id: {target_notebook_id}")
+    return target_notebook_id
 
-    with open(makrdown_file, mode='r', encoding='utf-8') as file_pointer:
+
+def upload_note(siyuan: Siyuan, notebook: str, markdown_file: str) -> bool:
+    """解析图片后上传笔记
+    :arg notebook: 笔记本ID
+    :arg markdown_file: 笔记路径
+    """
+    with open(markdown_file, mode='r', encoding='utf-8') as file_pointer:
         markdown = file_pointer.read()
 
     # 查找markdown中的本地图像, 上传, 替换路径
@@ -58,7 +55,7 @@ def main():
         if net_pattern.match(path):
             logger.debug(f"network resource {path}")
             continue
-        path = normcase(pathjoin(dirname(makrdown_file), path))
+        path = normcase(pathjoin(dirname(markdown_file), path))
 
         server_path = siyuan.upload_asset(path)
         logger.info(f"upload file {path} success")
@@ -66,11 +63,30 @@ def main():
         markdown = markdown.replace(matched_str, f"![{desc}]({server_path})")
 
     # 上传Markdown
-    document_name = basename(makrdown_file)
+    document_name = basename(markdown_file)
     document_id = siyuan.create_markdown_document(
-        target_notebook_id, document_name, markdown)
+        notebook, document_name, markdown)
     logger.info(f"uploaded document id: {document_id}")
+    return True
+
+
+@logger.catch
+def main():
+    """主函数
+    """
+    if len(argv) <= 2:
+        print(USAGE)
+        broken(129)
+
+    siyuan = Siyuan(URL, TOKEN, VERIFY)
+    target_notebook_id = choose_notebook(siyuan)
+
+    for file in argv[1:]:
+        upload_note(siyuan, target_notebook_id, file)
+
 
 
 if __name__ == "__main__":
+    logger.remove()
+    logger.add(stderr, level='INFO')
     main()
